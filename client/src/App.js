7c2fe820c5c826e19db4208a5ff7ef6d591fa402
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-10-04 10:20:50 -0600

    Commiting data rendered into a card

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  state = {
+    player: []
+  }
 
+  componentDidMount = () => {
+    this.playerData();
+  }
+
+  // Axios call to get player data
+  playerData = () => {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(response =>{
+      console.log("response", response.data)
+      this.setState({
+        player: response.data
+      })
+    })
+
+    .catch(error => {
+      console.log("Unable to fetch data", error)
+    })
+  }
+
+
+  // render fetched player data in a card
+  render(){
+    return (
+      <div className="App">
+        <h2>Google Women's World Cup Players</h2>
+        <div className="player-card">
+          {this.state.player.map(players => (
+            <div className="player-list">
+            <div className="player-cards" key={players.id}>
+              <p>Player Name: {players.name}</p>
+              <p>Country: {players.country}</p>
+              <p>Searches: {players.searches}</p>
+            </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+};
 export default App;
